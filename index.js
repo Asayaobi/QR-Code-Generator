fa3d2b914db13c2dbdb98577f36d0b89316afcd6
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-08-18 20:59:07 +0200

    Use qr-image turn the answer into qr code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import inquirer from 'inquirer'
-
+import qr from 'qr-image'
 
 //Use the inquirer npm package to get user input.
 inquirer.prompt([
@@ -10,4 +10,8 @@ inquirer.prompt([
        }])
       .then(function(answer){
         console.log(answer) //{ website: 'www.xxxxx.com' }
-      })
+
+//Use qr-image turn the answer into qr code
+        let qr_image = qr.image(answer.website, { type: "png" })
+    })
+
